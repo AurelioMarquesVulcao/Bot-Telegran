be978b1febdcaca35e5a922c9559f061bb324101
Aurelio <amarque@impacta.adv.br>
2021-03-02 16:25:55 -0300

    testes de porta

--- app/db.js
+++ app/db.js
@@ -3,6 +3,6 @@ const shell = require('shelljs');
 const sleep = require('await-sleep');
 
 (async () => {
-  shell.exec(`json-server --watch db.json`);
+  shell.exec(`json-server --watch db.json --port 3999`);
   await sleep(3000);
 })();
