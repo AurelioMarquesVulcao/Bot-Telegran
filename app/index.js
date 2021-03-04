46e87cdb9f664e32c5e90a8ad2d1266303c8a028
Aurelio <amarque@impacta.adv.br>
2021-03-04 13:22:13 -0300

    criação de tempo para ligar

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const shell = require('shelljs');
+const sleep = require('await-sleep');
 
 const { Listening } = require('./services/messages');
 
 (async () => {
   try {
+    await sleep(5000)
     new Listening().listen();
     // new Listening().lastMessage();
   } catch (e) {
